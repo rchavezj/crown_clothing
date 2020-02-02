10b2f222b3f1a80534dc75c7726187147a3aa1a6
OverRatedTech <rchavezj.ai@gmail.com>
2020-02-02 11:55:02 -0800

    Organizing images inside public folder

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.styles.scss';
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <Link to='/'>
+                
+            </Link>
+        </div>
+    );
+};
