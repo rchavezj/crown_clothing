54f08089164430b5128281f46c31b43a2ee045f2
OverRatedTech <rchavezj.ai@gmail.com>
2020-02-03 16:06:45 -0800

    authentication features

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -29,19 +32,19 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
-                        name='email' value={this.state.email} 
-                        required onChange={this.handleChange}
+                    <FormInput 
+                        name='email' type='email' value={this.state.email} 
+                        required label='email' handleChange={this.handleChange}
                     />
-                    <label>Email</label>
                     
-                    <input 
-                        name='password' value={this.state.password} 
-                        required onChange={this.handleChange}
+                    <FormInput 
+                        name='password' type='password' value={this.state.password} 
+                        required label='password' handleChange={this.handleChange}
                     />
-                    <label>Password</label>
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit' >
+                        Sign in
+                    </CustomButton>
                 </form>
             </div>
         )
