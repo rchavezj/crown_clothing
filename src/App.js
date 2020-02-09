7f915403821ca49335c6a6b5cebf4952184a52b6
OverRatedTech <rchavezj.ai@gmail.com>
2020-02-09 15:18:20 -0800

    Cleaning up the import libraries

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-// import { auth } from "./firebase/firebase.utils";
 
 import ShopPage from './pages/shop/shop.component';
 import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-// import { auth } from 'firebase';
 
 
 class App extends React.Component {
