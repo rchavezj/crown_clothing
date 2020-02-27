d508d5be2b58eb96bf4ab1255e37a8c9de194ee4
OverRatedTech <rchavezj.ai@gmail.com>
2020-02-26 18:59:42 -0800

    Added cartIcon and cartDropItem onto the React UI

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,13 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import CardIcon from '../cart-icon/cart-icon.components';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
       <div className="header">
         <Link className="logo-container" to="/">
@@ -22,7 +24,9 @@ const Header = ({ currentUser }) => {
             Contact
           </Link>
           {currentUser ? <SignOut /> : <SignIn />}
+          <CardIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
       </div>
     );
 };
@@ -40,8 +44,9 @@ const SignIn = () => (
 );
 
 // Function access the state
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  // currentUser: state.user.currentUser
+  currentUser, hidden
+});
 
 export default connect(mapStateToProps)(Header);
