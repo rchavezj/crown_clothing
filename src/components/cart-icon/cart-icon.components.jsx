321dbdb2fe880496eab5dc95bf3d2cb4f520e5a6
OverRatedTech <rchavezj.ai@gmail.com>
2020-03-01 13:34:40 -0800

    Updating packages and app with reselect yarn add reselect.

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
@@ -19,14 +20,10 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
-  console.log('I am being called');
+const mapStateToProps = (state) => {
   return {
-    itemCount: cartItems.reduce(
-      (accumalatedQuantity, cartItem) => 
-        accumalatedQuantity + cartItem.quantity,  0
-    )
-  }
+    itemCount: selectCartItemsCount(state)
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
