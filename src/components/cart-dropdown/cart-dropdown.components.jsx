f4b915e7c45edcdca1fe9e352d56a1c55bdd4135
OverRatedTech <rchavezj.ai@gmail.com>
2020-02-29 18:09:20 -0800

    Added redux features to keep track of our cart items with multiple UI componets

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items' />
+const CartDropdown = ({ cartItems }) => (
+    <div className="cart-dropdown">
+        <div className="cart-items">
+            {cartItems.map(
+                cartItem => (
+                    <CartItem 
+                        item={cartItem} 
+                        key={cartItem.id} 
+                    />
+                )
+            )}
+        </div>
         <CustomButton>Go to checkout</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const  mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
